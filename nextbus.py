687b20490b0f9e241a9038432ceadbe66b25ae9b
Claira P <8902046+ClairProgrammingUnit@users.noreply.github.com>
2019-08-20 17:26:09 -0500

    added JSON package, changed API response format
    Imported the JSON package from the standard Python library. Added a contant to append to all API calls that forces a JSON response format.

--- nextbus.py
+++ nextbus.py
@@ -1,9 +1,15 @@
 # v.0.1 Send requests to the Metro API
+
+# Libraries
+import json
 import urllib3
+
+# Append to URL to force JSON response format
+DOTJSON = "?format=json"
 
 def main():
     http = urllib3.PoolManager()
-    response = http.request('GET', "http://svc.metrotransit.org/")
+    response = http.request('GET', "http://svc.metrotransit.org/nextrip/providers" + DOTJSON)
     print(response.status)
 
 if __name__ == "__main__":
