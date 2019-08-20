42f31c27a1ae615c17fd54b576b2ac757baf404d
Claira P <8902046+ClairProgrammingUnit@users.noreply.github.com>
2019-08-20 18:01:17 -0500

    Parse JSON data from API
    Wrote a function to take the API output of a list of dictionaries and strip out unnecessary information. The new function returns a list.

--- nextbus.py
+++ nextbus.py
@@ -1,4 +1,4 @@
-# v.0.1 Send requests to the Metro API
+# v.0.2 Get JSON data from the Metro API
 
 # Libraries
 import json
@@ -7,10 +7,24 @@ import urllib3
 # Append to URL to force JSON response format
 DOTJSON = "?format=json"
 
-def main():
+# API -> JSON returns 'raw_data' as a list of dictionaries
+#    [{'Text': 'University of Minnesota', 'Value': '1'}, {'Text': 'Airport (MAC)', 'Value': '2'}, ...]
+# Filter 'raw_data' into a list of all 'Text' items
+def DictToList(listOfDicts):
+    newList = []
+    for dict in listOfDicts:
+        newList.append(dict['Text'])
+    return newList
+
+# Program Execution, keep at end of file
+def Main():
     http = urllib3.PoolManager()
     response = http.request('GET', "http://svc.metrotransit.org/nextrip/providers" + DOTJSON)
-    print(response.status)
+    raw_data = json.loads(response.data.decode('utf-8'))
+    formatted_data = DictToList(raw_data)
+    print(formatted_data)
+
+    return 0
 
 if __name__ == "__main__":
-    main();
+    Main();
