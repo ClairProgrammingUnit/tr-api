d9e2e3c5beac7dd0870019a2838a49634333d8f5
Claira P <8902046+ClairProgrammingUnit@users.noreply.github.com>
2019-08-20 17:12:29 -0500

    added urllib3 library
    Installed and imported the urllib3 library, adding the ability to send requests to the Metro API.

--- nextbus.py
+++ nextbus.py
@@ -1,7 +1,10 @@
-# v.0.0 Confirm Python setup
+# v.0.1 Send requests to the Metro API
+import urllib3
 
 def main():
-    print("Hello, world!")
+    http = urllib3.PoolManager()
+    response = http.request('GET', "http://svc.metrotransit.org/")
+    print(response.status)
 
 if __name__ == "__main__":
     main();
