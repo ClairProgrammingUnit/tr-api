5a73eee5faeb3ccdaa58a212e7491b0905b5a963
Claira P <8902046+ClairProgrammingUnit@users.noreply.github.com>
2019-08-20 19:31:28 -0500

    Updated JSON parser, added comm-line args
    Changed the JSON parser to return a simple dictionary as ID numbers were being lost with just a list. The new parser works to create dictionaries regardless of the API operation called. The program now also finds directions for a given route given command line arguments.

--- nextbus.py
+++ nextbus.py
@@ -1,7 +1,8 @@
-# v.0.2 Get JSON data from the Metro API
+# v.0.3 Find the directions available for a given route
 
 # Libraries
 import json
+import sys
 import urllib3
 
 # Append to URL to force JSON response format
@@ -9,20 +10,31 @@ DOTJSON = "?format=json"
 
 # API -> JSON returns 'raw_data' as a list of dictionaries
 #    [{'Text': 'University of Minnesota', 'Value': '1'}, {'Text': 'Airport (MAC)', 'Value': '2'}, ...]
-# Filter 'raw_data' into a list of all 'Text' items
-def DictToList(listOfDicts):
-    newList = []
+# Filter 'raw_data' into a single dictionary
+#    {'University of Minnesota':'1', 'Airport (MAC)':'2', ...}
+def DictToList(listOfDicts, text, id):
+    newDict = {}
     for dict in listOfDicts:
-        newList.append(dict['Text'])
-    return newList
+        newDict[dict[text]] = dict[id]
+    return newDict
 
 # Program Execution, keep at end of file
 def Main():
+    # Setup program info
+    args = sys.argv[1:]
     http = urllib3.PoolManager()
-    response = http.request('GET', "http://svc.metrotransit.org/nextrip/providers" + DOTJSON)
+
+    # Get routes
+    response = http.request('GET', "http://svc.metrotransit.org/NexTrip/Routes" + DOTJSON)
     raw_data = json.loads(response.data.decode('utf-8'))
-    formatted_data = DictToList(raw_data)
-    print(formatted_data)
+    route_dict = DictToList(raw_data, 'Description', 'Route')
+    chosen_route = route_dict[args[0]]
+
+    # Get directions
+    response = http.request('GET', "http://svc.metrotransit.org/NexTrip/Directions/" + chosen_route + DOTJSON)
+    raw_data = json.loads(response.data.decode('utf-8'))
+    direction_dict = DictToList(raw_data, 'Text', 'Value')
+    print(direction_dict)
 
     return 0
 
