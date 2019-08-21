45b496f3e41880ef14de16175869979bcacfa022
Claira P <8902046+ClairProgrammingUnit@users.noreply.github.com>
2019-08-20 20:59:15 -0500

    Expanded API usage, added constants
    Added API calls to get the bus stops given a route/direction and to get the departure times given a route/direction/stop. Added constants for command line arguments to keep random looking numbers out of the code.  Added string formatting to the API calls.

--- nextbus.py
+++ nextbus.py
@@ -1,12 +1,16 @@
-# v.0.3 Find the directions available for a given route
+# v.0.4 Get list of departure times for a given stop, route, and direction
 
 # Libraries
 import json
 import sys
 import urllib3
 
-# Append to URL to force JSON response format
+# Constants
 DOTJSON = "?format=json"
+FIRST_USER_ARG = 1
+ROUTE = 0
+STOP = 1
+DIRECTION = 2
 
 # API -> JSON returns 'raw_data' as a list of dictionaries
 #    [{'Text': 'University of Minnesota', 'Value': '1'}, {'Text': 'Airport (MAC)', 'Value': '2'}, ...]
@@ -18,23 +22,51 @@ def DictToList(listOfDicts, text, id):
         newDict[dict[text]] = dict[id]
     return newDict
 
+def FormatDirArg(arg):
+    newArg = ""
+    if arg == "north":
+        newArg = "NORTHBOUND"
+    elif arg == "east":
+        newArg = "EASTBOUND"
+    elif arg == "south":
+        newArg = "SOUTHBOUND"
+    elif arg == "west":
+        newArg = "WESTBOUND"
+    else:
+        newArg = "INVALID"
+    return newArg
+
 # Program Execution, keep at end of file
 def Main():
     # Setup program info
-    args = sys.argv[1:]
+    args = sys.argv[FIRST_USER_ARG:]
     http = urllib3.PoolManager()
 
+    args[DIRECTION] = FormatDirArg(args[DIRECTION])
+
     # Get routes
-    response = http.request('GET', "http://svc.metrotransit.org/NexTrip/Routes" + DOTJSON)
+    response = http.request('GET', "http://svc.metrotransit.org/NexTrip/Routes%s" % DOTJSON)
     raw_data = json.loads(response.data.decode('utf-8'))
     route_dict = DictToList(raw_data, 'Description', 'Route')
-    chosen_route = route_dict[args[0]]
+    chosen_route = route_dict[args[ROUTE]]
 
     # Get directions
-    response = http.request('GET', "http://svc.metrotransit.org/NexTrip/Directions/" + chosen_route + DOTJSON)
+    response = http.request('GET', "http://svc.metrotransit.org/NexTrip/Directions/%s%s" % (chosen_route, DOTJSON))
     raw_data = json.loads(response.data.decode('utf-8'))
     direction_dict = DictToList(raw_data, 'Text', 'Value')
-    print(direction_dict)
+    chosen_direction = direction_dict[args[DIRECTION]]
+
+    # Get stops
+    response = http.request('GET', "http://svc.metrotransit.org/NexTrip/Stops/%s/%s%s" % (chosen_route, chosen_direction, DOTJSON))
+    raw_data = json.loads(response.data.decode('utf-8'))
+    stop_dict = DictToList(raw_data, 'Text', 'Value')
+    chosen_stop = stop_dict[args[STOP]]
+
+    # Get departures for given stop
+    response = http.request('GET', "http://svc.metrotransit.org/NexTrip/%s/%s/%s%s" % (chosen_route, chosen_direction, chosen_stop, DOTJSON))
+    raw_data = json.loads(response.data.decode('utf-8'))
+    departure_dict = DictToList(raw_data, 'DepartureText', 'DepartureTime')
+    print(departure_dict)
 
     return 0
 
